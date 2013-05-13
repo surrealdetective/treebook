ccc3845c4b5c2e94848884fdca75d5fbbaa44872
Alex Au <thefifth@gmail.com>
2013-05-12 21:33:26 -0400

    Added profiles controller and adjusted routes by testing for profile_name

---
+++ test/functional/profiles_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ProfilesControllerTest < ActionController::TestCase
+  test "should get show" do
+  	get :show, id: users(:alex).profile_name
+    assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+end
